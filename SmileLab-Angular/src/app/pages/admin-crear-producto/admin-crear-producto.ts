5f73c2cd4d57cea8151d0682dc10e868429a07e8
Daniela <dmeliansalem@gmail.com>
2026-04-06 20:01:37 +0100

    Creación de angular.

---
+++ SmileLab-Angular/src/app/pages/admin-crear-producto/admin-crear-producto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-crear-producto',
+  imports: [],
+  templateUrl: './admin-crear-producto.html',
+  styleUrl: './admin-crear-producto.css',
+})
+export class AdminCrearProducto {}
