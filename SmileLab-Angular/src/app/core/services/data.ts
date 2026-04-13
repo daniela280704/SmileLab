75a1e7376912531595b75dbec54ecca204b8705b
oscar <oscar@ulpgc.es>
2026-04-13 20:25:15 +0100

    Editar data.ts

--- SmileLab-Angular/src/app/core/services/data.ts
+++ SmileLab-Angular/src/app/core/services/data.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class Data {
-  
+@Injectable({ providedIn: 'root' })
+export class DataService {
+  private http = inject(HttpClient);
+  private apiUrl = 'assets/db.json';
+
+  getInicio(): Observable<any> {
+    return this.http.get<any>(this.apiUrl).pipe(map(data => data.inicio));
+  }
+
+  getServicios(): Observable<any> {
+    return this.http.get<any>(this.apiUrl).pipe(map(data => data.servicios));
+  }
+
+  getEquipo(): Observable<any> {
+    return this.http.get<any>(this.apiUrl).pipe(map(data => data.equipo));
+  }
+
+  getProductos(): Observable<any[]> {
+    return this.http.get<any>(this.apiUrl).pipe(map(data => data.productos.items));
+  }
 }
