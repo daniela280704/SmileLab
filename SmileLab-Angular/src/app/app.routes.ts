95d18bb9558344fdb4b6f06fa9f02aa86d512ae3
oscar <oscar@ulpgc.es>
2026-04-13 19:39:35 +0100

    Generar componentes y servicios

--- SmileLab-Angular/src/app/app.routes.ts
+++ SmileLab-Angular/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Citas } from './pages/citas/citas';
 import { AdminCrearProducto } from './pages/admin-crear-producto/admin-crear-producto';
 import { Login } from './pages/login/login';
 
+
 export const routes: Routes = [
   { path: 'contacto', component: Contacto },
   { path: 'citas', component: Citas },
