3abc1ce4f9ae6b9c6c428e1f7404877bf6e11ead
oscar <oscar@ulpgc.es>
2026-04-21 17:55:16 +0100

    Arreglar merge

--- SmileLab-Angular/src/app/pages/productos/productos.ts
+++ SmileLab-Angular/src/app/pages/productos/productos.ts
@@ -1,18 +1,12 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { DataService } from '../../core/services/data';
-
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { DataService } from '../../core/services/data';
 
 @Component({
   selector: 'app-productos',
   standalone: true,
-<<<<<<< HEAD
-  imports: [CommonModule, RouterLink],
-=======
   imports: [CommonModule, RouterLink, RouterLinkActive],
->>>>>>> df2e572caa5c1b684272348d4959c6dd89c8013c
   templateUrl: './productos.html',
   styleUrl: './productos.css'
 })
