40f3bda02b8696b7421bd9f4718ad9212d4a8229
oscar <oscar@ulpgc.es>
2026-04-21 17:20:53 +0100

    Añadir ruta productos

--- SmileLab-Angular/src/app/app.routes.ts
+++ SmileLab-Angular/src/app/app.routes.ts
@@ -3,11 +3,10 @@ import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
 import { Equipo } from './pages/equipo/equipo';
 import { Servicios } from './pages/servicios/servicios';
+import { Productos } from './pages/productos/productos';
 import { Contacto } from './pages/contacto/contacto';
 import { Login } from './pages/login/login';
-
 import { PerfilComponent } from './pages/perfil/perfil.component';
-
 import { Citas } from './pages/citas/citas';
 import { AdminCrearProducto } from './pages/admin-crear-producto/admin-crear-producto';
 
@@ -17,11 +16,10 @@ export const routes: Routes = [
   { path: 'inicio', component: Inicio },
   { path: 'equipo', component: Equipo },
   { path: 'servicios', component: Servicios },
+  { path: 'productos', component: Productos },
   { path: 'contacto', component: Contacto },
   { path: 'login', component: Login },
-
   { path: 'perfil', component: PerfilComponent },
-
   { path: 'citas', component: Citas },
   { path: 'admin-crear-producto', component: AdminCrearProducto },
 
