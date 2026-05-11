302470aa1d3895425d51c9e6e4842ec742a728ac
Giuseppe <newgiuseppe241@gmail.com>
2026-05-11 20:14:11 +0100

    environment.prod.ts modidificado

--- SmileLab-Ionic/src/environments/environment.prod.ts
+++ SmileLab-Ionic/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB4I9rc1K00rDYxfi8ATTwsTGmm3B-ldmg",
+    authDomain: "smilelab-angular.firebaseapp.com",
+    projectId: "smilelab-angular",
+    storageBucket: "smilelab-angular.firebasestorage.app",
+    messagingSenderId: "436045669533",
+    appId: "1:436045669533:web:3a43f3661ed6cacc91e074"
+  }
 };
