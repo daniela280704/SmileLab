19bc58174a9341c289f77ca55a0f9083d787d239
Giuseppe <newgiuseppe241@gmail.com>
2026-05-11 19:51:37 +0100

    favoritos.ts modificado

--- SmileLab-Ionic/src/app/services/favoritos.ts
+++ SmileLab-Ionic/src/app/services/favoritos.ts
@@ -4,5 +4,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Favoritos {
-  
+
+  private favoritosIds: string[] = ['cepillo-electrico'];
+
+  async getFavoritosIds(): Promise<string[]> {
+    return this.favoritosIds;
+  }
+
+  async esFavorito(productoId: string): Promise<boolean> {
+    return this.favoritosIds.includes(productoId);
+  }
 }
